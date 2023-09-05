dc1e7e0ff169d0e3496665fb70a4788b894d3881
wowhyunnie <wowhyunnie@gmail.com>
2023-09-05 17:23:11 +0900

    feat: 로그인뷰 미완성

--- BinGongGan_User/BinGongGan_User/LoginMap/LoginView.swift
+++ BinGongGan_User/BinGongGan_User/LoginMap/LoginView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var emailText: String = ""
+    @State private var passwordText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Spacer()
+            
+            Image("LoginLogo")
+                .resizable()
+                .frame(width: 250, height: 250)
+            
+            Group{
+                TextField("이메일을 입력하세요", text: $emailText)
+                TextField("비밀번호를 입력하세요", text: $passwordText)
+            }
+            .padding()
+            .frame(width: 353, height: 47)
+            .background(Color.white)
+            .cornerRadius(15)
+            
+            
+            Button {
+                //
+            } label: {
+                Text("로그인")
+            }
+            .padding()
+            .frame(width: 353, height: 47)
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .cornerRadius(15)
+            
+            Spacer()
+            
+            Button {
+                //
+            } label: {
+                Text("회원가입")
+            }
+            .padding()
+            .frame(width: 100, height: 37)
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .cornerRadius(50)
+            
+            Text("@hanelCOMPANY")
+                .font(.caption)
+
+            Spacer()
+        }
     }
 }
 
