931a718402917c50c62181d6009d15b796f2664b
yoonjiho37 <yoonjiho37@github.com>
2023-09-05 15:16:00 +0900

    feat-file init

---
+++ BinGongGan_User/BinGongGan_User/LoginMap/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  BinGongGan_User
+//
+//  Created by 윤지호 on 2023/09/05.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
