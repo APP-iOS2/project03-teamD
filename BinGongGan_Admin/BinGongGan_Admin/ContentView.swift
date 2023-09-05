168b907d29a5211880f1c6f37305eece0d0dd64d
HANLeeeee <hanleeeee@naver.com>
2023-09-05 14:20:39 +0900

    fix: 타노스 복구

---
+++ BinGongGan_Admin/BinGongGan_Admin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BinGongGan_Admin
+//
+//  Created by 최하늘 on 2023/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
