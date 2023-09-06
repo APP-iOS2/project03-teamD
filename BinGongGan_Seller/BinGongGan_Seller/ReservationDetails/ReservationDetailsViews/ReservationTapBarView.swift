96d529cbc5e992a96cb0ef7fe756ef4bdf2a9383
minki-kim-git <rlaalsrl1227@gmail.com>
2023-09-06 13:11:58 +0900

    feat : 예약 내역 뷰 리스트 및 달력 추가

---
+++ BinGongGan_Seller/BinGongGan_Seller/ReservationDetails/ReservationDetailsViews/ReservationTapBarView.swift
@@ -0,0 +1,32 @@
+//
+//  ReservationTapBarView.swift
+//  BinGongGan_Seller
+//
+//  Created by 김민기 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct ReservationTapBarView : View {
+    
+    var reservationTapType : ReservationTapType
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            switch reservationTapType {
+            case .list:
+                ReservationDetailsListView()
+            case .calendar:
+                ReservationDetailsCalendarView()
+            }
+        }
+    }
+}
+
+
+struct ReservationTapBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReservationTapBarView(reservationTapType: .calendar)
+    }
+}
+
