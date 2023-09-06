afe194ed3e19ece96a1a5f25bdfa7fb1427fc309
hhh131 <gmlrnjs131@naver.com>
2023-09-06 18:31:43 +0900

    style: infomations 버튼 색상변경 및 사진 크기 조정

--- BinGongGan_Seller/BinGongGan_Seller/PlaceAdd/PlaceInfomationModel.swift
+++ BinGongGan_Seller/BinGongGan_Seller/PlaceAdd/PlaceInfomationModel.swift
@@ -12,17 +12,16 @@ struct PlaceInfomationModel: Identifiable {
     var id = UUID()
     var name: String
     var imageString: String
-    var color: Color
     var isSelected: Bool
     
     static let data = [
-        PlaceInfomationModel(name: "와이파이", imageString: "wifi",color: .red, isSelected: false),
-        PlaceInfomationModel(name: "주차장", imageString: "parkingsign",color: .black, isSelected: false),
-        PlaceInfomationModel(name: "반려동물", imageString: "pawprint.fill",color: .mySecondary, isSelected: false),
-        PlaceInfomationModel(name: "냉난방시설", imageString: "air.conditioner.horizontal",color: .blue, isSelected: false),
-        PlaceInfomationModel(name: "화재경보기", imageString: "flame.fill",color: .red, isSelected: false),
-        PlaceInfomationModel(name: "소화기", imageString: "person",color: .red, isSelected: false),
-        PlaceInfomationModel(name: "엘리베이터", imageString: "door.sliding.left.hand.closed",color: .myDarkGray,isSelected: false),
-        PlaceInfomationModel(name: "흡연실", imageString: "smoke",color: .myDarkGray,isSelected: false),
+        PlaceInfomationModel(name: "와이파이", imageString: "wifi", isSelected: false),
+        PlaceInfomationModel(name: "주차장", imageString: "parkingsign",isSelected: false),
+        PlaceInfomationModel(name: "반려동물", imageString: "pawprint.fill", isSelected: false),
+        PlaceInfomationModel(name: "냉난방시설", imageString: "air.conditioner.horizontal", isSelected: false),
+        PlaceInfomationModel(name: "화재경보기", imageString: "flame.fill", isSelected: false),
+        PlaceInfomationModel(name: "소화기", imageString: "person", isSelected: false),
+        PlaceInfomationModel(name: "엘리베이터", imageString: "door.sliding.left.hand.closed",isSelected: false),
+        PlaceInfomationModel(name: "흡연실", imageString: "smoke",isSelected: false),
     ]
 }
