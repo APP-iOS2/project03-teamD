bf2fc73914223396a4d16208a8869f0eb8d4f8fc
Tori <ttang1135@naver.com>
2023-09-06 11:54:47 +0900

    feat: 1:1문의 공사중 뷰

---
+++ BinGongGan_User/BinGongGan_User/MyPage/CustomerService/EnquiryView.swift
@@ -0,0 +1,20 @@
+//
+//  EnquiryView.swift
+//  BinGongGan_User
+//
+//  Created by 방유빈 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct EnquiryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EnquiryView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnquiryView()
+    }
+}
