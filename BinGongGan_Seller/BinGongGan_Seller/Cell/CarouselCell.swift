6f261dc55066a1274012eeec3f051f1b8fd2ff6a
kempt007 <133959724+kempt007@users.noreply.github.com>
2023-09-06 17:55:37 +0900

    feat: TabView 추가
    CarouseCell 추가
    MySpaceCell에 TabView 기능 추가

---
+++ BinGongGan_Seller/BinGongGan_Seller/Cell/CarouselCell.swift
@@ -0,0 +1,31 @@
+//
+//  CarouselCell.swift
+//  BinGongGan_Seller
+//
+//  Created by 윤경환 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct CarouselCell: View {
+    var body: some View {
+        TabView {
+            ForEach(1...3, id: \.self) { _ in
+                Image("image1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(15)
+                    .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
+                    .padding(.horizontal, 15)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+struct CarouselCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CarouselCell()
+    }
+}
