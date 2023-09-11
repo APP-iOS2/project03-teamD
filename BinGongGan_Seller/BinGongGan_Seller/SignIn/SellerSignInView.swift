e7a9f44b126cc8f5a78db0396c707d11f4271a6b
wowhyunnie <wowhyunnie@gmail.com>
2023-09-11 15:47:07 +0900

    feat: 판매자 로그인뷰 수정

--- BinGongGan_Seller/BinGongGan_Seller/SignIn/SellerSignInView.swift
+++ BinGongGan_Seller/BinGongGan_Seller/SignIn/SellerSignInView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct SellerSignInView: View {
-//    @StateObject private var signUpStore = SignUpStore()
+    //    @StateObject private var signUpStore = SignUpStore()
     @State private var emailText: String = ""
     @State private var passwordText: String = ""
     @State private var isShowingAlert: Bool = false
     
     @EnvironmentObject private var signInStore: SignInStore
-
+    
     var signIn: SignIn = SignIn()
     
     var body: some View {
@@ -23,44 +23,42 @@ struct SellerSignInView: View {
             Spacer().background(Color.myBackground).edgesIgnoringSafeArea(.all)
             
             VStack {
-                
                 Spacer()
                 
                 Image("SignInLogo")
                     .resizable()
                     .frame(width: 250, height: 250)
+                    .overlay(alignment: .bottom) {
+                        Text("Biz")
+                            .padding()
+                            .font(.head1Bold)
+                            .foregroundColor(.myPrimary)
+                    }
                 
-                Group {
-                    CustomTextField(placeholder: "이메일을 입력하세요", text: $emailText)
-                    CustomTextField(placeholder: "비밀번호를 입력하세요", text: $passwordText)
+                VStack {
+                    CustomTextField(placeholder: "이메일을 입력하세요", keyboardType: .emailAddress, text: $emailText)
+                    CustomSecureField(placeholder: "비밀번호를 입력하세요", text: $passwordText)
                 }
-                .padding()
-                .frame(width: 353, height: 47)
-                .background(Color.white)
-                .cornerRadius(15)
+                .padding(.horizontal, 20)
                 
                 
-                Button {
-//                    isShowingAlert = signInStore.checkSignIn(email: emailText, password: passwordText)
-                } label: {
-                    Text("로그인")
-                }
-                .padding()
-                .frame(width: 353, height: 47)
-                .foregroundColor(.white)
-                .background(Color.myPrimary)
-                .cornerRadius(15)
+                
+                PrimaryButton(action: {
+                    isShowingAlert = signInStore.checkSignIn(email: emailText, password: passwordText)
+                }, title: "로그인")
+                .padding(.horizontal, 20)
                 
                 Spacer()
                 
                 NavigationLink {
-//                    SignUpView()
-//                        .environmentObject(signUpStore)
+                    //                    SignUpView()
+                    //                        .environmentObject(signUpStore)
                 } label: {
-                    Text("회원가입")
+                    Text("Biz 회원가입")
                 }
                 .padding()
-                .frame(width: 100, height: 37)
+                .font(.body1Bold)
+                .frame(width: 120, height: 40)
                 .foregroundColor(.white)
                 .background(Color.mySecondary)
                 .cornerRadius(50)
@@ -79,14 +77,15 @@ struct SellerSignInView: View {
             }
         }
         .ignoresSafeArea()
-//        .onAppear {
-//            signUpStore.currentStep = .first
-//        }
+        //        .onAppear {
+        //            signUpStore.currentStep = .first
+        //        }
     }
 }
 
-struct SellerSignInView_Previews: PreviewProvider {
+struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
         SellerSignInView()
+            .environmentObject(SignInStore())
     }
 }
