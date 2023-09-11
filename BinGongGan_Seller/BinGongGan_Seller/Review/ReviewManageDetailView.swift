f146d3250da18fa0406df4cb1f61fb92cc4fbe27
Chae-young Park <pcy07111@gmail.com>
2023-09-11 11:06:56 +0900

    style: Delete a ZStack for background

--- BinGongGan_Seller/BinGongGan_Seller/Review/ReviewManageDetailView.swift
+++ BinGongGan_Seller/BinGongGan_Seller/Review/ReviewManageDetailView.swift
@@ -16,37 +16,30 @@ struct ReviewManageDetailView: View {
     @State var isShowingReviewReportSheet: Bool = false
     
     var body: some View {
-        ZStack {
-            Color.myBackground
-                .ignoresSafeArea(.all)
-            
-            VStack {
-                ScrollView {
+        VStack {
+            ScrollView {
                     ReservationCell()
                         .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
                     
                     ReviewWithCommentCell()
                         .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                    
                 }
-                
-                Button("리뷰 신고하기", role: .destructive) {
-                    isShowingReviewReportSheet.toggle()
-                }
+            
+            Button("리뷰 신고하기", role: .destructive) {
+                isShowingReviewReportSheet.toggle()
             }
-            .navigationTitle("리뷰 상세")
-            .navigationBarTitleDisplayMode(.inline)
         }
+        .background(Color.myBackground)
+        .navigationTitle("리뷰 상세")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollContentBackground(.hidden)
+        .customBackbutton()
         .sheet(isPresented: $isShowingReviewReportSheet) {
             ReviewReportSheet(isShowingReviewReportSheet: $isShowingReviewReportSheet)
                 .environmentObject(reportStore)
                 .presentationDetents([.medium, .large])
         }
-        .navigationBarBackButtonHidden(true)
-        .scrollContentBackground(.hidden)
-        .background(Color.myBackground)
-        .navigationBarTitleDisplayMode(.inline)
-        .customBackbutton()
     }
 }
 
