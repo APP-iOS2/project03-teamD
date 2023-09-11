0b8b98b75e80917dd1ac7e2341899cbbb7ccb21a
Kyoungmi <1mmgm.dev@gmail.com>
2023-09-11 11:48:50 +0900

    [feature]: modify the toast message

--- BinGongGanCore/Sources/BinGongGanCore/Extensions/Namespace/Color+Extension.swift
+++ BinGongGanCore/Sources/BinGongGanCore/Extensions/Namespace/Color+Extension.swift
@@ -46,4 +46,6 @@ public extension Color {
     static let myErrorColor = Color(hex: "#FFCDD2", opacity: 0.5)
     
     static let myVeryLightGrayColor = Color(hex: "#EBEEF2")
+    
+    static let myWarningColor = Color(hex: "#FED875")
 }
