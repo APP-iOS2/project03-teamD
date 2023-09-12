8fdf0c7d2caa388abed6866add1306e39e9ffd6b
Chae-young Park <pcy07111@gmail.com>
2023-09-12 10:46:18 +0900

    feat: Create a PlaceDetailView

---
+++ BinGongGan_Seller/BinGongGan_Seller/Place/PlaceDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PlaceDetailView.swift
+//  BinGongGan_Seller
+//
+//  Created by 박채영 on 2023/09/12.
+//
+
+import SwiftUI
+
+struct PlaceDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaceDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaceDetailView()
+    }
+}
