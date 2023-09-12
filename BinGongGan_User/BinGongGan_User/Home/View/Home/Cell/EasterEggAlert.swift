c4a8e64c119f7ae492c80f66cd0d1a58014720d6
Jehyeon Lee <dasom8899981@gmail.com>
2023-09-13 01:12:54 +0900

    [리팩토링]

--- BinGongGan_User/BinGongGan_User/Home/View/Home/Cell/EasterEggAlert.swift
+++ BinGongGan_User/BinGongGan_User/Home/View/Home/Cell/EasterEggAlert.swift
@@ -8,48 +8,48 @@
 import SwiftUI
 
 struct EasterEggAlert: View {
-
-  @Binding var isPresented: Bool
-  let title: String
-  let primaryButtonTitle: String
-  let primaryAction: () -> Void
-
-  var body: some View {
-    VStack(spacing: 22) {
-      Image("mungmoongE")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 80)
-
-      Text(title)
-        .font(.title)
-        .bold()
-        .foregroundColor(.black)
-
-      Divider()
-      Button {
-        primaryAction()
-        isPresented = false
-      } label: {
-        Text(primaryButtonTitle)
-          .font(.title3)
-          .bold()
-          .padding(.vertical, 8)
-          .frame(maxWidth: .infinity)
-      }
-      .buttonStyle(.borderedProminent)
-    }
-    .padding(.horizontal, 24)
-    .padding(.vertical, 18)
-    .frame(width: 300)
-    .background(
-      RoundedRectangle(cornerRadius: 30)
-        .stroke(.blue.opacity(0.5))
+    @EnvironmentObject var homeStore: HomeStore
+    @Binding var isPresented: Bool
+    let title: String
+    let primaryButtonTitle: String
+    let primaryAction: () -> Void
+    var body: some View {
+        VStack(spacing: 22) {
+            
+            Image("\(homeStore.mungImage[homeStore.mungImageCount])")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
+            
+            Text(title)
+                .font(.title)
+                .bold()
+                .foregroundColor(.black)
+            
+            Divider()
+            Button {
+                primaryAction()
+                isPresented = false
+            } label: {
+                Text(primaryButtonTitle)
+                    .font(.title3)
+                    .bold()
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding(.horizontal, 24)
+        .padding(.vertical, 18)
+        .frame(width: 300)
         .background(
-          RoundedRectangle(cornerRadius: 30)
-            .fill(.white)
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(.blue.opacity(0.5))
+                .background(
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(.white)
+                )
         )
-    )
-  }
+    }
 }
 
