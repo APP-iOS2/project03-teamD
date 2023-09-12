92aaf1781b9c50f42851f0097da383c9b9faf09c
Tori <ttang1135@naver.com>
2023-09-12 11:14:10 +0900

    fix: Review모델 작성자 아이디 추가

--- BinGongGanCore/Sources/BinGongGanCore/Model/Review.swift
+++ BinGongGanCore/Sources/BinGongGanCore/Model/Review.swift
@@ -10,6 +10,7 @@ import Foundation
 public struct Review: Identifiable {
     public var id: String = UUID().uuidString
     public var placeId: String //리뷰 달린 공간 id값
+    public var writerId: String
     public var date: Double = Date().timeIntervalSince1970 //작성 날짜
     public var rating: Double //별점
     public var content: String //내용
