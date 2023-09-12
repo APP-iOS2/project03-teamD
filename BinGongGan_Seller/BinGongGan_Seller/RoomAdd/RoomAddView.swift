a01897886c4673157609b32141f07685c6ca2345
hhh131 <gmlrnjs131@naver.com>
2023-09-13 03:17:28 +0900

    feat: RoomViews Add

---
+++ BinGongGan_Seller/BinGongGan_Seller/RoomAdd/RoomAddView.swift
@@ -0,0 +1,89 @@
+//
+//  RoomAddView.swift
+//  BinGongGan_Seller
+//
+//  Created by 신희권 on 2023/09/11.
+//
+
+import SwiftUI
+import BinGongGanCore
+import Combine
+
+struct RoomAddView: View {
+    @EnvironmentObject var roomStore:RoomStore
+    @State private var selectedImage: [UIImage] = []
+    
+    
+    var body: some View {
+        ZStack {
+            Color.myBackground
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Group{
+                        Text("방이름")
+                        CustomTextField(placeholder: "방이름을 입력하세요", text: $roomStore.room.name)
+                    }
+                    
+                    Group {
+                        Text("공간 대여 가격")
+                        HStack(spacing: 0) {
+                            Text("시간 당")
+                                .padding(.trailing, 5)
+                                .foregroundColor(Color.myDarkGray)
+                            
+                            TextField("가격을 입력하세요", text: $roomStore.room.price)
+                                .keyboardType(.decimalPad)
+                                .onReceive(Just(roomStore.room.price)) { newValue in
+                                    let filtered = newValue.filter { "0123456789".contains($0) }
+                                    if filtered != newValue {
+                                        self.roomStore.room.price = filtered
+                                    }
+                                }
+                                .overlay(alignment:.trailing) {
+                                    Text("￦")
+                                        .padding(.trailing, 1)
+                                }
+                            
+                        }
+                        .padding()
+                        .background(.white)
+                        .cornerRadius(10)
+                    }
+                    .padding(.top, 15)
+//                    
+//                    Section {
+//                        Text("방 상세사진")
+//                        PhotoSelectedView(selectedImages: $selectedImage, selectedImageNames: $roomStore.room.imageNames)
+//                    }
+//                    
+                    Section{
+                        Text("방 상세내용")
+                        TextEditor(text: $roomStore.room.note)
+                            .font(.body1Regular)
+                            .frame(height: 300)
+                            .cornerRadius(10)
+                    }
+                    
+                    PrimaryButton(title: "등록 하기") {
+                        roomStore.addRoom(placeId: "heekwon")
+                    }
+                }
+            }
+          
+            .padding(20)
+        }
+        .customBackbutton()
+        .navigationTitle("방 추가")
+    }
+}
+
+struct RoomAddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            RoomAddView()
+                .environmentObject(RoomStore())
+        }
+    }
+}
---
+++ BinGongGan_Seller/BinGongGan_Seller/RoomAdd/RoomStore.swift
@@ -0,0 +1,63 @@
+//
+//  RoomStore.swift
+//  BinGongGan_Seller
+//
+//  Created by 신희권 on 2023/09/12.
+//
+
+import Foundation
+import BinGongGanCore
+import FirebaseFirestore
+
+class RoomStore: ObservableObject {
+    @Published var room: Room = Room()
+    @Published var rooms: [Room] = []
+    let placeStore: PlaceStore = PlaceStore()
+    let dataBase = Firestore.firestore().collection("Room")
+    
+    func addRoom(placeId: String) {
+        room.placeId = placeId
+        dataBase.document(room.id)
+            .setData(room.asDictionary())
+        print("방 추가 완료")
+    }
+    
+    func updateRoom(roomId: String) {
+        dataBase.document(room.id)
+            .setData(room.asDictionary())
+        print("방 추가 완료")
+    }
+    
+    func fetchRooms() {
+        dataBase.whereField("placeId", isEqualTo: "heewkwon").getDocuments { (document, error) in
+            self.rooms = []
+            if let error = error {
+                print("Error fetching data: \(error)")
+            } else {
+                for document in document!.documents {
+                    if let rooms = try? document.data(as: Room.self) {
+                        self.rooms.append(rooms)
+                    } else {
+                        print("Error")
+                    }
+                }
+            }
+        }
+    }
+    
+    func fetchRoom() {
+        dataBase.whereField("placeId", isEqualTo: "heekwon").getDocuments { (document, error) in
+            if let error = error {
+                print("Error fetching data: \(error)")
+            } else {
+                for document in document!.documents {
+                    if let room = try? document.data(as: Room.self) {
+                        self.room = room
+                    } else {
+                        print("Error")
+                    }
+                }
+            }
+        }
+    }
+}
