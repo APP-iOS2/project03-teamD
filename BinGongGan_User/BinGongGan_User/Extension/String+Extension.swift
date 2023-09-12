11c202c0d2c0278ae4c57a9f297d43b9d460a119
SonYoonho <yoonhoson@naver.com>
2023-09-12 16:41:13 +0900

    feat: add Bank + BankSelectView

--- BinGongGan_User/BinGongGan_User/Extension/String+Extension.swift
+++ BinGongGan_User/BinGongGan_User/Extension/String+Extension.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 extension String {
-    // MARK: 숫자로만된 전화번호를 '-'가 합쳐진 문자열로 변환
+    /// 숫자로만된 전화번호를 '-'가 포함된 문자열로 변환
     func formatPhoneNumber() -> String {
-        guard self.count == 11 else { return self } // 11자리가 아니면 그대로 반환
+        guard self.count == 11 else { return self }
         
         let firstPart = self.prefix(3)
         let secondPart = self.dropFirst(3).prefix(4)
@@ -18,4 +18,23 @@ extension String {
         
         return "\(firstPart)-\(secondPart)-\(thirdPart)"
     }
+    
+    /// 숫자로된 6자리 생년월일을 '-'가 포함된 문자열로 반환
+    func formatBirthDate() -> String {
+        let year = self.prefix(2)
+        let month = self.dropFirst(2).prefix(2)
+        let date = self.dropFirst(4).prefix(2)
+        
+        let currentYear = Calendar.current.component(.year, from: Date())
+        
+        var fullYear: String
+        if let shortYear = Int(year), shortYear >= (currentYear % 100) {
+            fullYear = "19\(year)"
+        } else {
+            fullYear = "20\(year)"
+        }
+        
+        return "\(fullYear)-\(month)-\(date)"
+    }
 }
+
