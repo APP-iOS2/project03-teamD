8573e2b21a3a0361d07b8ce179cba6b01f3f0ed0
Jehyeon Lee <dasom8899981@gmail.com>
2023-09-14 04:12:08 +0900

    style: 이미지 기다리는것들은 전부 스켈레톤 함

--- BinGongGan_User/BinGongGan_User/Home/View/Home/FavoriteListView.swift
+++ BinGongGan_User/BinGongGan_User/Home/View/Home/FavoriteListView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FavoriteListView: View {
     
     @EnvironmentObject var homeStore: HomeStore
-    
+    @State var isLoading: Bool = true
     var body: some View {
         TabView {
             ForEach(homeStore.hotPlace) { place in
@@ -49,6 +49,14 @@ struct FavoriteListView: View {
                         .cornerRadius(8, corners: .bottomLeft)
                         .cornerRadius(8, corners: .bottomRight)
                     }// ZSTACK
+                    .onAppear {
+                        Task {
+                            await homeStore.fetchPlaces()
+                            self.isLoading = false
+                        }
+                    }
+                    .redacted(reason: isLoading ? .placeholder :
+                                [])
                 }
             }.padding(.horizontal, 5)
         }// TABVIEW
