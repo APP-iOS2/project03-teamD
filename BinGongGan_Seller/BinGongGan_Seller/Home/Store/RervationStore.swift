4ddf437082dfb62e8dd19d2e411392305814d409
minki-kim-git <rlaalsrl1227@gmail.com>
2023-09-14 13:06:58 +0900

    fix : 예약내역상태변경

--- BinGongGan_Seller/BinGongGan_Seller/Home/Store/RervationStore.swift
+++ BinGongGan_Seller/BinGongGan_Seller/Home/Store/RervationStore.swift
@@ -36,17 +36,13 @@ final class RervationStore : ObservableObject{
             self.data = try snapshot.documents.compactMap {
                 try $0.data(as: SellerReservation.self)
             }
-//            print(data)
             filterData(completion: completion)
         } catch {
             print("Error fetching reviews: \(error)")
         }
     }
-//    public var reservationDateString: String {
-//        return
-//    }
     func filterData(completion: @escaping (Bool) -> ()){
-       let mydata = data.filter { data in
+        let mydata = data.filter { data in
             data.placeID == sellerUid
         }
         
@@ -55,7 +51,7 @@ final class RervationStore : ObservableObject{
         }
         
         let canceldata = data.filter{
-            $0.reservationState == 3
+            $0.reservationState == 4
             //여기서 나온 값들 중에 최신값
         }
         self.canceldata = canceldata
@@ -66,7 +62,7 @@ final class RervationStore : ObservableObject{
 
         self.waitldata = waitldata
         let confilmedldata = data.filter{
-            $0.reservationState == 1 || $0.reservationState == 2
+            $0.reservationState == 1 || $0.reservationState == 2 || $0.reservationState == 3
         }
         self.confilmedldata = confilmedldata
         completion(true)
@@ -74,11 +70,11 @@ final class RervationStore : ObservableObject{
     func updateRervation(id:String,isReserve:Bool) async {
         let dataBase = Firestore.firestore().collection("Reservation")
         do {
-        //TODO: 여기 값만 넣어주기
+            //TODO: 여기 값만 넣어주기
             try await dataBase
                 .document(id)
                 .updateData([
-                    "reservationState": isReserve ? 1 : 3
+                    "reservationState": isReserve ? 1 : 4
                 ])
             
             await fetchData { success in
@@ -93,89 +89,89 @@ final class RervationStore : ObservableObject{
         }
     }
 }
-    
-    
 
 
- struct SellerReservation: Identifiable,Codable {
-     let id: String //= UUID().uuidString
-     let userEmail: String
-     let roomID: String
-   
-     let reservationYear: String
-     let reservationMonth: String
-     let reservationDay: String
+
+
+struct SellerReservation: Identifiable,Codable {
+    let id: String //= UUID().uuidString
+    let userEmail: String
+    let roomID: String
     
-     var reservationDateString: String {
+    let reservationYear: String
+    let reservationMonth: String
+    let reservationDay: String
+    
+    var reservationDateString: String {
         return "\(reservationYear)-\(reservationMonth)-\(reservationDay)"
     }
     
-     let checkInYear: String
-     let checkInMonth: String
-     let checkInDay: String
+    let checkInYear: String
+    let checkInMonth: String
+    let checkInDay: String
     
     public var checkInDateString: String {
         return "\(checkInYear)/\(checkInMonth)/\(checkInDay)"
     }
     
-     let checkOutYear: String
-     let checkOutMonth: String
-     let checkOutDay: String
+    let checkOutYear: String
+    let checkOutMonth: String
+    let checkOutDay: String
     
     public var checkOutDateString: String {
         return "\(checkOutYear)/\(checkOutMonth)/\(checkOutDay)"
     }
-     
-     let placeID : String
-     let hour: Int
-     let personnel: Int
     
-     let reservationName: String
-     let reservationPhoneNumber: String
-     let reservationRequest: String
-     let reservationState: Int
-     
-     
-     var calcDate : String? {
-         let today = Date()
-         let dateFormatter = DateFormatter()
-         dateFormatter.dateFormat = "yyyy-MM-dd"
-         let targetDate = dateFormatter.date(from: reservationDateString) ?? Date()
-         let calendar = Calendar.current
-         let dateComponents = calendar.dateComponents([.day], from:targetDate , to: today)
-         
-         // 차이를 출력합니다.
-         if let days = dateComponents.day {
-             if days == 0{
-                 return "오늘"
-             }
-             return "\(days)일 전"
-         } else {
-             return nil
-         }
-     }
-     enum CodingKeys: CodingKey {
-         case id
-         case userEmail
-         case roomID
-         case reservationYear
-         case reservationMonth
-         case reservationDay
-         case checkInYear
-         case checkInMonth
-         case checkInDay
-         case checkOutYear
-         case checkOutMonth
-         case checkOutDay
-         case placeID
-         case hour
-         case personnel
-         case reservationName
-         case reservationPhoneNumber
-         case reservationRequest
-         case reservationState
-     }
-
+    let placeID : String
+    let hour: Int
+    let personnel: Int
+    
+    let reservationName: String
+    let reservationPhoneNumber: String
+    let reservationRequest: String
+    let reservationState: Int
+    
+    
+    var calcDate : String? {
+        let today = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let targetDate = dateFormatter.date(from: reservationDateString) ?? Date()
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.day], from:targetDate , to: today)
+        
+        // 차이를 출력합니다.
+        if let days = dateComponents.day {
+            if days == 0{
+                return "오늘"
+            }
+            return "\(days)일 전"
+        } else {
+            return nil
+        }
+    }
+    enum CodingKeys: CodingKey {
+        case id
+        case userEmail
+        case roomID
+        case reservationYear
+        case reservationMonth
+        case reservationDay
+        case checkInYear
+        case checkInMonth
+        case checkInDay
+        case checkOutYear
+        case checkOutMonth
+        case checkOutDay
+        case placeID
+        case hour
+        case personnel
+        case reservationName
+        case reservationPhoneNumber
+        case reservationRequest
+        case reservationState
+    }
+    
 }
 
 public enum ReservationHistoryType: String , CaseIterable {
